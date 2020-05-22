e2c8765f90ef046f0d204073a846b2898fcb38a2
Abdullah Elsheikh <Abdullahelsheikh@gmail.com>
2020-05-22 19:47:49 +0200

    K15

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+
+import pandas as pd
+from sklearn.externals import joblib
+from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
