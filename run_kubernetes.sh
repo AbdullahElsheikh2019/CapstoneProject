988316a267d40d0c33258fbae579bc2a5f7780bb
Abdullah Elsheikh <Abdullahelsheikh@gmail.com>
2020-05-22 19:31:03 +0200

    kubernetes 12

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,10 +9,9 @@ dockerpath="372839978247.dkr.ecr.eu-west-1.amazonaws.com/udacity:latest"
 # Step 2
 # Run the Docker Hub container with kubernetes
 kubectl run app\
-    --generator=apps/v1\
     --image=$dockerpath\
     --port=80 --labels app=app
-
+#     --generator=apps/v1\
 # Step 3:
 # List kubernetes pods
 kubectl get pods
